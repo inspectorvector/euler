d7f256ccd1cbbee6d28045f8b42f6201c57ac8e9
inspectorvector <malakian@gmail.com>
2014-08-29 20:03:28 +0100

    progressing..

--- 012.py
+++ 012.py
@@ -8,13 +8,15 @@ def highly_divisible_tri_num():
     current_tri_num = 0
     list_of_divisors = []
 
-    while len(list_of_divisors) < 501:
+    while len(list_of_divisors) < 6:
         current_tri_num += 1
         list_of_divisors = []
-        for i in range(1, triangle_number_generator(current_tri_num) + 1):
+        value_to_test = triangle_number_generator(current_tri_num)
+        for i in range(1, value_to_test + 1):
             if current_tri_num % i == 0:
                 list_of_divisors.append(i)
     print(list_of_divisors)
-    return current_tri_num
+    print(current_tri_num)
+    return value_to_test
 
 print(highly_divisible_tri_num())
