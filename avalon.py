a6cafe9247de34fb4347f8a36c4d47134a44841a
inspectorvector <alex@localhost.(none)>
2014-09-02 22:08:10 +0100

    progressing

--- avalon.py
+++ avalon.py
@@ -5,17 +5,21 @@ from threading import Timer
 # Good and evil ratio for player amounts: if 5: 3g 2e, if 6: 4g 2e, if 7: 4g 3e, if 8: 5g 3e, if 9: 6g 3e, if 10: 6g 4e
 # initially, special roles will be Merlin and Assassin only. Others will simply be Loyal servants of Arthur or the Minions of Mordred.
 
-@willie.module.commands('init')
-def set_up(bot, trigger):
-    bot.say('hello?')
+@willie.module.commands('init') # manually initializes stuff, as i couldn't get setup working.
+def initialise(bot, trigger):
+    bot.say('Initialised!')
     bot.seats_available = True
     bot.game_runner = None
     bot.people_seated = []
+    bot.lsoa = [] # loyal servants of arthur: the good team
+    bot.mom = [] # minions of mordred: the evil team
+    bot.merlin = None
+    bot.assassin = None
 
 @willie.module.commands('avalon')
 def avalon(bot, trigger):
     if not bot.seats_available:
-        bot.say('a game of Avalon is already underway.')
+        bot.say('A game of Avalon is already underway.')
         return
     bot.game_runner = trigger.nick
     bot.say(bot.game_runner + ' wants to play Avalon. Type .join to take a seat at the round table. ' + bot.game_runner + ', type .start when everyone is ready to begin the game.')
@@ -36,7 +40,18 @@ def join(bot, trigger):
 @willie.module.commands('start')
 def start(bot, trigger):
     if bot.game_runner == trigger.nick:
-        bot.say('Welcome to Avalon')
+        bot.say('Welcome to a new game of Avalon!')
+        bot.seats_available = False
+        if len(bot.people_seated) < 5:
+            bot.say('Sorry, not enough people are at the round table for a game to be played.')
+            initialise(bot, trigger)
+        if len(bot.people_seated) == 5:
+        if len(bot.people_seated) == 6:
+        if len(bot.people_seated) == 7:
+        if len(bot.people_seated) == 8:
+        if len(bot.people_seated) == 9:
+        if len(bot.people_seated) == 10:
     else:
         bot.say('You are not the current game runner, or a game of Avalon has not been started.')
 
+def
