78aab1efa6b26e1696f6950d9c55c256382ed047
inspectorvector <malakian@gmail.com>
2014-09-04 18:07:56 +0100

    progressing..

--- avalon.py
+++ avalon.py
@@ -5,12 +5,14 @@ from threading import Timer
 # Good and evil ratio for player amounts: if 5: 3g 2e, if 6: 4g 2e, if 7: 4g 3e, if 8: 5g 3e, if 9: 6g 3e, if 10: 6g 4e
 # initially, special roles will be Merlin and Assassin only. Others will simply be Loyal servants of Arthur or the Minions of Mordred.
 
+# fix game state stuff, SORT NOTIFICATIONS - evils need to know other evils
+
 @willie.module.commands('init') # manually initializes stuff, as i couldn't get setup working.
 def initialise(bot, trigger):
     bot.say('Initialised. You may now begin a new game of avalon. Type .avalon to start.')
     bot.seats_available = True
     bot.game_leader = None
-    bot.people_seated = []
+    bot.people_seated = ['malakian', 'Sid', 'Jimmeh', 'pugi', 'MangoFusion']
     bot.lsoa = [] # loyal servants of arthur: the good team
     bot.mom = [] # minions of mordred: the evil team
     bot.merlin = None
@@ -21,7 +23,7 @@ def initialise(bot, trigger):
 
 @willie.module.commands('avalon')
 def avalon(bot, trigger):
-    if not bot.seats_available:
+    if bot.game_state == 'NOGAME':
         bot.say('A game of Avalon is already underway.')
         return
     bot.game_leader = trigger.nick
@@ -51,11 +53,7 @@ def stand(bot, trigger):
     else:
         bot.say('You may not leave the round table at this time.')
 
-@willie.module.commands('kingmaker')
-def king(bot, trigger):
-    bot.turn_order = bot.people_seated
-    random.shuffle(bot.turn_order)
-    bot.say(bot.turn_order[0] + ' , you shall be the first king!')
+
 
 @willie.module.commands('start')
 def start(bot, trigger):
@@ -65,12 +63,25 @@ def start(bot, trigger):
         allocate_teams(bot, trigger)
         if not bot.seats_available:
             for name in bot.mom:
-                bot.msg(name + ', you are a Minion of Mordred (evil team)')
+                if name == bot.assassin:
+                    bot.msg(name, name + ', you are the feared ASSASSIN! (evil team). Tell no one.')
+                else:
+                    bot.msg(name, name + ', you are a Minion of Mordred (evil team). Your team mates are ' + 'Do not tell anyone!')
             for name in bot.lsoa:
-                bot.msg(name + ', you are a Loyal Servant of Arthur (good team)')
+                if bot.merlin == name:
+                    bot.msg(name, name + ', you are the wizard Merlin (good team). The evil team members are ' + bot.mom[0:] + '. Do NOT reveal yourself! If the Minions of Mordred guess your identity, evil will win the game!')
+                bot.msg(name, name + ', you are a Loyal Servant of Arthur (good team).')
+        first_king(bot, trigger)
 
     else:
         bot.say('You are not the current game leader, or a game of Avalon has not been started.')
+
+
+def first_king(bot, trigger):
+    bot.turn_order = bot.people_seated
+    random.shuffle(bot.turn_order)
+    bot.say(bot.turn_order[0] + ' , you shall be the first king!')
+
 
 def allocate_teams(bot, trigger):
     random.shuffle(bot.people_seated)
